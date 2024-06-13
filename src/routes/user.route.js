9d14b2a78ce7c2590814bee7043ec20d223bb748
Kareen Ujjainiya <kareenujjainiya222@gmail.com>
2024-06-13 21:53:47 +0530

    Added more routes for user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,12 @@ import {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelDetails,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,4 +37,13 @@ router.route("/logout").post(verifyJWT, logoutUser);
 // verifyJWT work is already handled in refreshAccessToken controller that's why it is not used here
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelDetails);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+
 export default router;
